4b88b819d4d1dab8516af53ab32eab9e7692600e
jiaqili610 <j.li.51@warwick.ac.uk>
2022-03-10 22:34:35 +0000

    Update 6.2_mixture_models_EM_algorithm.R
    update

--- 6.2_mixture_models_EM_algorithm.R
+++ 6.2_mixture_models_EM_algorithm.R
@@ -27,4 +27,82 @@ Z = rbinom(500,1,0.75)
 
 # sample from mixture model 111
 
-x <- rnorm(10000, mean=mu.true[Z+1], sd=sigma.true[Z+1])
+X <- rnorm(10000, mean=mu.true[Z+1], sd=sigma.true[Z+1])
+
+hist(X, breaks = 15)
+
+
+
+
+
+
+# since we now assume parameters are known, mixture components are fully specified
+# for each observation we compute the likelihood P(Xi | Zi= 0) and P(Xi | Zi=1).
+# We store these values in the columns of L: 
+# we write a function to compute the log weighted  likelihood
+compute.log.lik <- function(L, w){
+        L[,1] = L[,1] * w[1]
+        L[,2] = L[,2] * w[2]
+        return(sum(log(rowSums(L))))
+}
+
+
+L= matrix(NA, nrow=length(X), ncol=2)
+
+L[,1] = dnorm(X, mean= mu.true[1], sd = sigma.true[1])
+L[,2] = dnorm(X, mean= mu.true[2], sd = sigma.true[2])
+
+
+
+# finally, we implement the E and M step in the EM.iter function below.
+
+# the mixture.EM function is the driver which checks for convergence by computing
+# the log likelihoods at each step
+
+mixture.EM <- function(w.init, L) {
+        
+        w.curr <- w.init
+        
+        # store log-likehoods for each iteration
+        log_liks <-c()
+        ll <- compute.log.lik(L, w.curr)
+        log_liks <- c(log_liks, ll)
+        delta.ll <-1
+        
+        while(delta.ll >1e-5){
+                w.curr <- EM.iter(w.curr, L)
+                ll     <- compute.log.lik(L, w.curr)
+                log_liks <- c(log_liks, ll)
+                delta.ll <- log_liks[length(log_liks)] - log_liks[length(log_liks)-1]
+        }
+        
+        return(list(w.curr, log_liks))
+}
+
+
+
+EM.iter <- function(w.curr, L, ...){
+        # E step: compute E_{Z| X,w0}[I(Z_i=k)]
+        z_ik <- L
+        
+        for(i in seq_len(ncol(L))) {
+                z_ik[,i] <- w.curr[i]*z_ik[,i]
+        }
+        
+        z_ik <- z_ik/rowSums(z_ik)
+        
+        # M-step
+        w.next <- colSums(z_ik)/sum(z_ik)
+        
+        return(w.next)
+        
+}
+
+
+
+
+#perform EM
+ee <- mixture.EM(w.init = c(0.5, 0.5), L)
+
+
+print(paste("Estimate = (", round(ee[[1]][1], 2), ",", round(ee[[1]][2], 2), ")", sep="" ))
