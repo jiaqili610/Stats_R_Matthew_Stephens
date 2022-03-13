1d615d068d3a9ed73eb837d0e3c223866e22c333
jiaqili610 <j.li.51@warwick.ac.uk>
2022-03-13 12:29:15 +0000

    update

---
+++ Inference/1.4_asymptotic_normality_MLE_bernoulli_poisson.R
@@ -0,0 +1,30 @@
+# Asymptotic Normality of MLE
+# simulation to observe the normality of MLE estimates
+
+rm(list = ls()) 
+
+
+################################################################################
+# Bernoulli
+################################################################################
+
+
+# simulation samples J = 7000
+# in each sample, size n=100
+# draw from Bernoulli, with true parameter p0=0.4
+
+p0 <- 0.4
+
+p0_mat = matrix(data=p0, nrow=7000,ncol=1)
+
+# function of bernoulli draw
+binomg <- function(x) {
+        rbinom(100,1,x)
+}
+
+sample_simulation <- sapply(p0_mat, binomg) 
+
+mle_p <- apply(sample_simulation, 2, sum)
+
+hist(mle_p/100)
+
